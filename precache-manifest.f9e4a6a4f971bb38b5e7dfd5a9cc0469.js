0a75869d1ae658b53d93fb792539eea4bcdb6740
Daniel Heydari <daniel.heydari1986@gmail.com>
2020-08-25 12:32:34 +0200

    Updates

---
+++ precache-manifest.f9e4a6a4f971bb38b5e7dfd5a9cc0469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b931c42298904a69eec6dab533ab94fa",
+    "url": "/REACT-Gasto-Semanal/index.html"
+  },
+  {
+    "revision": "80aa57a23dfce7308d3b",
+    "url": "/REACT-Gasto-Semanal/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "d4b147e06cfaf1b3dc3f",
+    "url": "/REACT-Gasto-Semanal/static/js/2.deeab028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-Gasto-Semanal/static/js/2.deeab028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80aa57a23dfce7308d3b",
+    "url": "/REACT-Gasto-Semanal/static/js/main.8f043bbf.chunk.js"
+  },
+  {
+    "revision": "17eec838c4ab5f388b43",
+    "url": "/REACT-Gasto-Semanal/static/js/runtime-main.e389fb9a.js"
+  }
+]);
